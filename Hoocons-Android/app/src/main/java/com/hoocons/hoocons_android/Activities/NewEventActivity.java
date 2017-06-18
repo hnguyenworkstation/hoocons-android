f53a0a5606e1289680315cf06d6238d96b955752
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-18 14:22:00 -0400

    added post event activity with header

---
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/Activities/NewEventActivity.java
@@ -0,0 +1,15 @@
+package com.hoocons.hoocons_android.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hoocons.hoocons_android.R;
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
