927b9f27de3c94c6f1970ddb2dfc47251fcc099b
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-07-15 10:46:35 -0400

    temporary added new meeting layout

--- Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/Activities/NewMeetingActivity.java
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/Activities/NewMeetingActivity.java
@@ -3,10 +3,17 @@ package com.hoocons.hoocons_android.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.facebook.rebound.BaseSpringSystem;
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringSystem;
+import com.facebook.rebound.SpringUtil;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
@@ -14,11 +21,13 @@ import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 import com.hoocons.hoocons_android.CustomUI.view.ViewHelper;
 import com.hoocons.hoocons_android.Managers.BaseActivity;
 import com.hoocons.hoocons_android.R;
+import com.hoocons.hoocons_android.ViewHolders.SquaredImageViewHolder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class NewMeetingActivity extends BaseActivity implements ObservableScrollViewCallbacks {
+public class NewMeetingActivity extends BaseActivity implements
+        ObservableScrollViewCallbacks, View.OnClickListener {
     @BindView(R.id.obs_scrollview)
     ObservableScrollView mScrollView;
     @BindView(R.id.custom_toolbar)
@@ -30,11 +39,18 @@ public class NewMeetingActivity extends BaseActivity implements ObservableScroll
     @BindView(R.id.linear)
     View mCustomBarLinear;
 
+    @BindView(R.id.submit_new_meeting)
+    Button mSubmitMeeting;
+
     private View mOverlayView;
     private int mActionBarSize;
     private int mFlexibleSpaceImageHeight;
 
     private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+
+    private final BaseSpringSystem mSpringSystem = SpringSystem.create();
+    private final ImageSpringListener springListener = new ImageSpringListener();
+    private Spring mScaleSpring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +59,11 @@ public class NewMeetingActivity extends BaseActivity implements ObservableScroll
         ButterKnife.bind(this);
 
         initGeneralView();
+        initOnClickListener();
+    }
+
+    private void initOnClickListener() {
+        mSubmitMeeting.setOnClickListener(this);
     }
 
     private void initGeneralView() {
@@ -58,6 +79,27 @@ public class NewMeetingActivity extends BaseActivity implements ObservableScroll
             @Override
             public void run() {
                 mScrollView.scrollTo(mFlexibleSpaceImageHeight, 0);
+            }
+        });
+
+        // Init submit button affect
+        // Create the animation spring.
+        mScaleSpring = mSpringSystem.createSpring();
+        mScaleSpring.addListener(springListener);
+
+        mSubmitMeeting.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mScaleSpring.setEndValue(0.3);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        mScaleSpring.setEndValue(0);
+                        break;
+                }
+                return true;
             }
         });
     }
@@ -105,5 +147,30 @@ public class NewMeetingActivity extends BaseActivity implements ObservableScroll
     @Override
     public void onUpOrCancelMotionEvent(ScrollState scrollState) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submit_new_meeting:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private class ImageSpringListener extends SimpleSpringListener {
+        @Override
+        public void onSpringUpdate(Spring spring) {
+            // On each update of the spring value, we adjust the scale of the image view to match the
+            // springs new value. We use the SpringUtil linear interpolation function mapValueFromRangeToRange
+            // to translate the spring's 0 to 1 scale to a 100% to 50% scale range and apply that to the View
+            // with setScaleX/Y. Note that rendering is an implementation detail of the application and not
+            // Rebound itself. If you need Gingerbread compatibility consider using NineOldAndroids to update
+            // your view properties in a backwards compatible manner.
+            float mappedValue = (float) SpringUtil.mapValueFromRangeToRange(spring.getCurrentValue(), 0, 1, 1, 0.5);
+            mSubmitMeeting.setScaleX(mappedValue);
+            mSubmitMeeting.setScaleY(mappedValue);
+        }
     }
 }
