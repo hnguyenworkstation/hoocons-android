908cff8b6d528b6dbc9ae9dd52445a8acc63b9a3
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-07-30 13:35:03 -0400

    added meetout activity

---
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/Activities/MeetOutActivity.java
@@ -0,0 +1,15 @@
+package com.hoocons.hoocons_android.Activities;
+
+import android.os.Bundle;
+
+import com.hoocons.hoocons_android.Managers.BaseActivity;
+import com.hoocons.hoocons_android.R;
+
+public class MeetOutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meet_out);
+    }
+}
