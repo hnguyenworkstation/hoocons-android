a4bd35b8387e64a1bf09cab888f43c0c024a5aae
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-08-20 19:09:35 -0400

    added messaging fragments recyclers

--- Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/ViewFragments/MessagingFragment.java
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/ViewFragments/MessagingFragment.java
@@ -2,42 +2,35 @@ package com.hoocons.hoocons_android.ViewFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hoocons.hoocons_android.R;
+import com.vstechlab.easyfonts.EasyFonts;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessagingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MessagingFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    @BindView(R.id.communicate_unread_textview)
+    TextView mUnreadTitle;
+    @BindView(R.id.communicate_older_textview)
+    TextView mRecentConvsTitle;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-
     public MessagingFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessagingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MessagingFragment newInstance(String param1, String param2) {
         MessagingFragment fragment = new MessagingFragment();
         Bundle args = new Bundle();
@@ -63,4 +56,26 @@ public class MessagingFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_messaging, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        initView();
+        initRecyclers();
+        initTypeFaces();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initRecyclers() {
+
+    }
+
+    private void initTypeFaces() {
+        mUnreadTitle.setTypeface(EasyFonts.robotoBold(getContext()));
+        mRecentConvsTitle.setTypeface(EasyFonts.robotoBold(getContext()));
+    }
 }
