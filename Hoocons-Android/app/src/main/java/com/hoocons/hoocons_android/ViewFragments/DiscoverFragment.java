0a35b046652bd72dd2d90944feef3de262c71e8d
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-08-27 14:23:20 -0400

    added list followed topics

--- Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/ViewFragments/DiscoverFragment.java
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/ViewFragments/DiscoverFragment.java
@@ -1,6 +1,7 @@
 package com.hoocons.hoocons_android.ViewFragments;
 
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,10 +13,17 @@ import android.support.v7.widget.SnapHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hoocons.hoocons_android.Adapters.CommentsAdapter;
 import com.hoocons.hoocons_android.Adapters.DiscoverTopPanelAdapter;
 import com.hoocons.hoocons_android.R;
+import com.vstechlab.easyfonts.EasyFonts;
+
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +31,11 @@ import butterknife.ButterKnife;
 public class DiscoverFragment extends Fragment {
     @BindView(R.id.top_panel)
     RecyclerView mTopPanelRecycler;
+
+    @BindView(R.id.followed_topics)
+    LinearLayout mFollowedTopicLayout;
+
+    private String[] tempTopicsArray;
 
 
     private DiscoverTopPanelAdapter mPanelAdapter;
@@ -42,6 +55,7 @@ public class DiscoverFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+
         }
     }
 
@@ -57,7 +71,39 @@ public class DiscoverFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
+        tempTopicsArray = getResources().getStringArray(R.array.channel_category);
+
         initTopPanelRecycler();
+        initFollowedTopicsView();
+    }
+
+    private void initFollowedTopicsView() {
+        int[] androidColors = getResources().getIntArray(R.array.array_topic_color);
+
+        for (int i = 0; i < tempTopicsArray.length; i++) {
+            final LinearLayout topicView = (LinearLayout) getLayoutInflater().inflate(R.layout.general_topic_view_layout,
+                    mFollowedTopicLayout, false);
+            TextView topicName = (TextView) topicView.findViewById(R.id.topic_text);
+
+            // Set topic random color
+            topicView.setBackgroundResource(R.drawable.general_rounded_shape);
+            GradientDrawable drawable = (GradientDrawable) topicView.getBackground();
+            drawable.setColor(androidColors[new Random().nextInt(androidColors.length)]);
+
+            // Set Topic text
+            topicName.setText(tempTopicsArray[i]);
+            topicName.setTypeface(EasyFonts.robotoRegular(getContext()));
+            topicView.setTag(i);
+
+            mFollowedTopicLayout.addView(topicView);
+            topicView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int i = (int) view.getTag();
+                    Toast.makeText(getContext(), "clicked: " + tempTopicsArray[i], Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     private void initTopPanelRecycler() {
