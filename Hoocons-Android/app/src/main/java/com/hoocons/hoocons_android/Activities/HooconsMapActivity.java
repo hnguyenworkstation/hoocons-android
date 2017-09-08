50053f6aa8d95178844cf3eb1fa11c80e1112698
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-09-08 14:21:11 -0400

    add hoocons map activity

---
+++ Hoocons-Android/app/src/main/java/com/hoocons/hoocons_android/Activities/HooconsMapActivity.java
@@ -0,0 +1,16 @@
+package com.hoocons.hoocons_android.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hoocons.hoocons_android.Managers.BaseActivity;
+import com.hoocons.hoocons_android.R;
+
+public class HooconsMapActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoocons_map);
+    }
+}
